e8929cdaaa6f679a0fc37964c8ea0bf0bc6e8620
Alexey Savinov <belsat.net@gmail.com>
2017-09-14 20:28:05 +0300

    word frequency

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,16 @@
+import collections
+import sys
 
 
 def load_data(filepath):
-    pass
+    words_list = open(filepath, encoding='utf-8').read().strip().split(' ')
+    return words_list
 
 
-def get_most_frequent_words(text):
-    pass
+def get_most_frequent_words(text_data):
+    return collections.Counter(text_data).most_common(10)
 
 
 if __name__ == '__main__':
-    pass
+    filename = sys.argv[1]
+    print(get_most_frequent_words(load_data(filename)))
