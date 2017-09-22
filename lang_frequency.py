a6d99957b8de5ff633f3768df436287eb8f1d22a
Alexey Savinov <belsat.net@gmail.com>
2017-09-22 14:43:08 +0300

    code decomposition

--- lang_frequency.py
+++ lang_frequency.py
@@ -3,14 +3,24 @@ import sys
 
 
 def load_data(filepath):
-    words_list = open(filepath, encoding='utf-8').read().strip().split()
+    with open(filepath, "r", encoding='utf-8') as input_file:
+        raw_text = input_file.read()
+    return raw_text
+
+
+def get_words_as_list(raw_text):
+    words_list = raw_text.strip().split()
     return words_list
 
 
-def get_most_frequent_words(text_data):
-    return collections.Counter(text_data).most_common(10)
+def get_n_most_frequent_words(text_data, count):
+    return collections.Counter(text_data).most_common(count)
 
 
 if __name__ == '__main__':
-    filename = sys.argv[1]
-    print(get_most_frequent_words(load_data(filename)))
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+        raw_data = load_data(filename)
+        print(get_n_most_frequent_words(get_words_as_list(raw_data), 10))
+    else:
+        print('No data for analyse')
